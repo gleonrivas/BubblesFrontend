8c6f8178c92c26f0a2fa68aa046989b200aa5867
gonzalo <gleonrivas@safareyes.es>
2023-01-16 19:42:59 +0100

    tamañito

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css',
+    './app.component.default.css',
+    './app.component.desktop.css',
+    './app.component.mobile.css']
 })
 export class AppComponent {
   title = 'frontendBubble';
