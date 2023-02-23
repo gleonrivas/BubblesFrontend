be23a4c9559af58bcb99b443c7812e50783f2298
celim <celimva@gmail.com>
2023-02-23 16:21:49 +0100

    crear publicacion sin acabar

--- src/app/views/pages/crear-publicacion/crear-publicacion.component.ts
+++ src/app/views/pages/crear-publicacion/crear-publicacion.component.ts
@@ -4,16 +4,16 @@ import {ActivatedRoute, Router} from '@angular/router';
 import {PublicacionService} from "../../../shared/services/publicacion.service";
 import {PublicacionParaCrear} from "../../../shared/models/publicacion/PublicacionParaCrear";
 import {FormBuilder, FormControl, FormGroup, FormsModule, Validators} from '@angular/forms';
-type TipoPublicacion = "texto" | "imagen" | "texto e imagen";
+type TipoPublicacion = "texto" | "imagen" ;
 @Component({
   selector: 'app-crear-publicacion',
   templateUrl: './crear-publicacion.component.html',
   styleUrls: ['./crear-publicacion.component.css']
 })
 export class CrearPublicacionComponent {
-  public readonly TIPOS_DE_PUBLICACION: TipoPublicacion[] = ['texto' , 'imagen' , 'texto e imagen'];
+  public readonly TIPOS_DE_PUBLICACION: TipoPublicacion[] = ['texto' , 'imagen' ];
   public id_perfil: number = -1;
-  public tipo_publicacion: TipoPublicacion = "texto e imagen";
+  public tipo_publicacion: TipoPublicacion = "texto";
   public texto: string = "";
   public imagen: string = "";
   public tematica: string = "";
@@ -25,17 +25,19 @@ export class CrearPublicacionComponent {
     texto: ['', [Validators.required, Validators.maxLength(500)]],
     imagen: ['', [Validators.required]],
     tematica: ['', [Validators.required]],
+    tipo_publicacion: ['', [Validators.required]]
 
   })
 
   private publicacion: PublicacionParaCrear = {
-    tipoPublicacion: this.tipo_publicacion,
-    texto: this.texto,
-    imagen: this.imagen,
+    tipoPublicacion:this.tipo_publicacion,
+    texto:this.texto,
+    imagen:this.imagen,
     tematica: this.tematica,
     fechaPublicacion: this.fecha_publicacion,
     activa: this.activa,
-    id_perfil: this.id_perfil
+    id_perfil: this.idPerfil(),
+
   };
 
 
@@ -57,22 +59,24 @@ export class CrearPublicacionComponent {
 
   }
 
-  onSubmit(){
-    console.log(this.publicationForm.value);
-    this.publicationForm.reset()
-  }
-  guardarPublicacion() {
-    console.log(this.publicacion)
+  idPerfil():number{
+    let idDevuelto = -1;
+    this.router.paramMap.subscribe((value) => {
+      const id = value.get('id_perfil');
 
-    this.publicacionService.crearPublicacion(this.publicacion).subscribe({
-      complete: () => {
-        this.route.navigateByUrl('/home')
-      },
-      error: () => {
-        this.loginError = true
+      if (id !== null) {
+         idDevuelto = parseInt(id);
+
       }
     });
+    return idDevuelto;
   }
+
+  onSubmit(){
+    console.log(this.publicationForm.value)
+    this.publicationForm.reset()
+  }
+
 
   fechaFormateada(): string {
     let nuevaFecha = moment();
@@ -88,6 +92,27 @@ export class CrearPublicacionComponent {
     const target = event.target as HTMLInputElement;
     this.tematica = target.value;
   }
+  onTextoInput(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.texto = target.value;
+  }
 
+    onImageInput(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.imagen = target.value;
+  }
+
+  guardarPublicacion() {
+    console.log(this.publicacion)
+
+    this.publicacionService.crearPublicacion(this.publicacion).subscribe({
+      complete: () => {
+        this.route.navigateByUrl('/home')
+      },
+      error: () => {
+        this.loginError = true
+      }
+    });
+  }
 
 }
